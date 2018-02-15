3e9abac6f1589aff9092837ae1ce7ef9ad90fedf
PengInGitHub <felixdeutschland@hotmail.com>
2018-02-15 21:28:52 +0100

    create bool var to judge wether to print values

--- Chapter_1_Introduction/Q2.go
+++ Chapter_1_Introduction/Q2.go
@@ -12,7 +12,10 @@ func PrintFizzBuzz(){
     const(
         FIZZ = 3
         BUZZ = 5        
-)    
+    )  
+   
+    var p bool //to judge wether to print the values
+    
     
     fmt.Println()
 }

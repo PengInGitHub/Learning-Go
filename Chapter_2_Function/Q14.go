7650aa4d1c196b0ea64914ac1dce0c991ca9e630
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-17 16:08:52 +0100

    use closures and finish Q14

--- Chapter_2_Function/Q14.go
+++ Chapter_2_Function/Q14.go
@@ -8,7 +8,14 @@ func PlusTwo() func(int) int{
     return func(x int) int {return x+2}
 }
 
+//use closures
+func PlusX(x int) func(int) int{
+    return func(y int) int{return x+y}
+}
+
 func PrintFunctionReturnsFunction(){
     p2 := PlusTwo()
     fmt.Printf("%v\n",p2(20))
+    p20 := PlusX(20)
+    fmt.Printf("%v\n",p20(20))    
 }

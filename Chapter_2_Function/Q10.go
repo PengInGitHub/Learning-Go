5e99da396d467e3adc6df8b8ed2e34e8d16932e2
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-17 15:03:02 +0100

    finish Q10 PrintFibonacci()

--- Chapter_2_Function/Q10.go
+++ Chapter_2_Function/Q10.go
@@ -5,8 +5,17 @@ import (
 
 )
 
+func fibonacci(value int) []int{
+    fibonacciArray := make([]int,value)
+    fibonacciArray[0],fibonacciArray[1] = 1, 1
+    for n:=2; n<value; n++ {
+        fibonacciArray[n] = fibonacciArray[n-1] + fibonacciArray[n-2]
+    }
+    return fibonacciArray
+
+}
+
 func PrintFibonacci(){
     
-    
-    fmt.Println()
+    fmt.Println(fibonacci(50))
 }

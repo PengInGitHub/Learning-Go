c577a73bd8f6fa774464bb12e3accbd59b2d523e
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-17 14:48:40 +0100

    init Q10 PrintFibonacci()

---
+++ Chapter_2_Function/Q10.go
@@ -0,0 +1,12 @@
+package main 
+
+import (
+"fmt"
+
+)
+
+func PrintFibonacci(){
+    
+    
+    fmt.Println()
+}
--- Chapter_2_Function/main.go
+++ Chapter_2_Function/main.go
@@ -3,5 +3,5 @@ package main
 //import "fmt"
 
 func main() {
-     PrintNumbers(2,3,75,43,19,2)
+     PrintFibonacci()
 }

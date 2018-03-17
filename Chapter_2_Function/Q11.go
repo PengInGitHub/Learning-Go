20f24fddb2165462cc5d9e15fc4b333144347df9
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-17 15:20:25 +0100

    finish Q11 map()

--- Chapter_2_Function/Q11.go
+++ Chapter_2_Function/Q11.go
@@ -5,8 +5,20 @@ import (
 
 )
 
+func Map(f func (int) int , l []int) []int{
+    j := make([]int, len(l))
+    for k,v := range l {
+        j[k] = f(v)
+    }
+    return j
+}
+
 func PrintMap(){
+    f := func(i int) int{
+        return i*i
+    }
+    m := []int{7,8,9}
+    mapResults := Map(f,m)
     
-    
-    fmt.Println()
+    fmt.Println(mapResults)
 }

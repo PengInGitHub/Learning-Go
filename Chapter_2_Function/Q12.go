89f12099a7efcf47727048870921e796b24f5abd
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-17 15:35:19 +0100

    finish Q12 Min() Max()

--- Chapter_2_Function/Q12.go
+++ Chapter_2_Function/Q12.go
@@ -4,9 +4,28 @@ import (
 "fmt"
 
 )
+func Min(l []int) (min int){
+    min = l[0]
+    for _,v := range l {
+        if v<min {
+            min = v
+        }
+    } 
+    return
+}
+
+func Max(l []int) (max int){
+    max = l[0]
+    for _,v := range l{
+        if v>max {
+            max = v
+        }
+    }
+    return
+}
 
 func PrintMinMax(){
-    
-    
-    fmt.Println()
+    a := []int{2,7,1,89,45}
+    fmt.Println(Max(a))    
+    fmt.Println(Min(a))
 }

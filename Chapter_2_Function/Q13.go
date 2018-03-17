cab8ce95881272a34e9b99e1cf1d0cb7462bf755
PengInGitHub <felixdeutschland@hotmail.com>
2018-03-17 15:49:21 +0100

    finish Q13 BubbleSort()

--- Chapter_2_Function/Q13.go
+++ Chapter_2_Function/Q13.go
@@ -5,8 +5,20 @@ import (
 
 )
 
+func BubbleSort(l []int){
+    for i:=0;i<len(l);i++ {
+        for j:=i+1;j<len(l);j++{
+            if l[j]<l[i] {
+                l[j], l[i] = l[i],l[j]
+            }
+        }
+    }
+}
+
 func PrintBubbleSort(){
     
-    
-    fmt.Println()
+    a := []int{2,100,19,6,32,82}
+    fmt.Printf("unsorted: %v\n",a)
+    BubbleSort(a)
+    fmt.Printf("sorted: %v\n",a)
 }
